5e5961d4f5b0d97232eb90ee4dcd27c1751f1354
Aryaman Saini <aryamansaini10@gmail.com>
2024-06-06 18:56:31 +0530

    completed basic sequelize setup for creation of DB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {PORT}=require('./configs/serverconfig');
+const {PORT}=require('./config/serverconfig');
 
 const setupandstartserver = async () => {
     const app = express();
